79ba040bfdeec966a5c3f2fa73043360759d3a59
unknown <aibhisarkar123@gmail.com>
2024-03-19 19:30:32 +0530

    new changes

--- App.tsx
+++ App.tsx
@@ -20,7 +20,6 @@ import {
 import {
   Colors,
   DebugInstructions,
-  Header,
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
@@ -71,23 +70,15 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next: Aibhi
+           India no 1 food delivery dining app
           </Section>
           <LearnMoreLinks />
         </View>
